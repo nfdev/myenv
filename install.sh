d3abe8d8792ab7830bcffbf6c88149c80a0a3409
nfdev <nf.devel@gmail.com>
2019-04-14 11:52:06 +0000

    Change vim scheme

--- install.sh
+++ install.sh
@@ -6,5 +6,5 @@ curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update && sudo apt-get install yarn
 
-mv ~/myenv/templates/.gitconfig ~/
+cp ~/myenv/templates/gitconfig ~/.gitconfig
 mv ~/myenv ~/.myenv
